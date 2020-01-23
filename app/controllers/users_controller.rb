1c5b8bf078b54ab4d9eeb2531d67d59374059502
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2020-01-23 16:59:58 -0600

    Add user friends listing feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-    def my_portfolio
-        @tracked_stock = current_user.stocks.order(last_price: :desc)
-    end
+
+  def my_portfolio
+    @tracked_stock = current_user.stocks.order(last_price: :desc)
+  end
+
+  def my_friends
+    @tracked_friend = current_user.friends.order(:name)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,36 @@
 class User < ApplicationRecord
-    has_many :user_stocks
-    has_many :stocks, through: :user_stocks
-    has_many :friendships
-    has_many :friends, through: :friendships
+  has_many :user_stocks
+  has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
 
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
-    def can_track_stock?(ticker_symbol)
-      under_stock_limit? && !stock_already_tracked?(ticker_symbol)
-    end
+  def can_track_stock?(ticker_symbol)
+    under_stock_limit? && !stock_already_tracked?(ticker_symbol)
+  end
 
-    def under_stock_limit?
-      stocks.count < 10
-    end
+  def under_stock_limit?
+    stocks.count < 10
+  end
 
-    def stock_already_tracked?(ticker_symbol)
-      stock = Stock.check_db(ticker_symbol)
-      return false unless stock
-      stocks.where(id: stock.id).exists?
-    end
+  def stock_already_tracked?(ticker_symbol)
+    stock = Stock.check_db(ticker_symbol)
+    return false unless stock
+    stocks.where(id: stock.id).exists?
+  end
+
+  def friend_already_tracked?(email)
+    friend = Friend.check_db(email)
+    return false unless friend
+    friends.where(id: friends.id).exists?
+  end
+
+  def can_track_friend?(email)
+    !friend_already_tracked?(email)
+  end
 
 end
