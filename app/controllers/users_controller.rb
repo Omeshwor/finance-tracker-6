db8f8ceaaefce449d25eb256ddf7628698283768
Omeshwor Shrestha <omeshworlalshrestha@yahoo.com>
2020-01-26 17:40:08 -0600

    Add user friend search frontend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,32 @@ class UsersController < ApplicationController
   end
 
   def my_friends
-    @tracked_friend = current_user.friends.order(:name)
+    @tracked_friend = current_user.friends.order(:created_at)
+  end
+
+  def search_friend
+    render json: params[:friend]
+  end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+        if @friend
+          respond_to do |format|
+            format.js { render partial: 'users/stock_result' }
+          end
+        else
+          respond_to do |format|
+            flash.now[:alert] = "Please enter a valid email"
+            format.js { render partial: 'users/stock_result' }
+          end
+        end
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a friend to be searched !!"
+        format.js { render partial: 'users/stock_result' }
+      end
+    end
   end
 
 end
